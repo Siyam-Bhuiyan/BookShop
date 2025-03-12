dfe7bd1782e9400fd7d1bbfdf3e88ded5ac7c20a
Siyam <bhuiyansiyam294@gmail.com>
2025-03-12 06:05:57 +0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT||3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors')
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+//connect mongodb
+const uri = process.env.MONGODB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //create db and collections
+    const db = client.db("BOOKSHOP");
+    const boooksCollection = db.collection("books");
+
+    //create a new book(post)
+    app.post('/books', async (req, res) => {
+        const bookData = req.body;
+        console.log(bookData);
+        try {
+            const result = await boooksCollection.insertOne(bookData);
+            res.send({
+                success: true,
+                message: 'Book added successfully'
+            });
+        } catch (error) {
+            res.status(500).send({
+                success: false,
+                message: error.message
+            });
+        }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Book Management API')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+//admin
+//5RF4kIyTygr6Fi4N
